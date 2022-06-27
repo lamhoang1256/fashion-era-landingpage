3d15653d0bc9fba9c313abb0e7909b4d513346c8
lamhoang1256 <lamhoang1256@gmail.com>
2022-06-27 20:36:13 +0700

    Feat: add collection,sale section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       colors: {
         black32: "#323232",
         yellowfb: "#FBD103",
+        green00: "#004743",
+      },
+      backgroundImage: {
+        "baby-pattern": "url('/images/baby-pattern.png')",
+      },
+      gradientColorStops: {
+        gradientBlue: "radial-gradient(64% 195.84% at 96.3% 9%, #6CDDFF 0%, #58C4E4 100%)",
       },
     },
   },
