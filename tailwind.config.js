be20169642bb54174ac2cbef9fe34f037cab15aa
lamhoang1256 <lamhoang1256@gmail.com>
2022-06-29 11:31:18 +0700

    Feat: add brand, trending, reviews section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,11 @@ module.exports = {
         black32: "#323232",
         yellowfb: "#FBD103",
         green00: "#004743",
+        green74: "#74CA4D",
       },
       backgroundImage: {
         "baby-pattern": "url('/images/baby-pattern.png')",
+        "review-bg": "url('/images/review-bg.png')",
       },
       gradientColorStops: {
         gradientBlue: "radial-gradient(64% 195.84% at 96.3% 9%, #6CDDFF 0%, #58C4E4 100%)",
